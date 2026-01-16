fe6a26e18b90ac5cd105fa1bca6dc4ec86767c53
qkrekdls <gunmannduu@gmail.com>
2026-01-16 21:25:59 +0900

    CMT. 폰트 수정

--- src/pages/Overview/CMT.tsx
+++ src/pages/Overview/CMT.tsx
@@ -5,7 +5,7 @@ function CMT() {
     <div className="flex-1 flex justify-center items-start mt-[150px] lg:mt-[100px]">
       <div className="flex flex-col items-center justify-center gap-[22px] lg:gap-[50px]">
         <div className="flex flex-col gap-[24px] items-center justify-center">
-          <h1 className="font-bakbak text-[40px] lg:text-[80px] leading-[28px] lg:leading-[60px] text-[#74CFFB]">Feedback</h1>
+          <h1 className="font-bakbak text-[40px] lg:text-[60px] leading-[28px] lg:leading-[60px] text-[#74CFFB]">Feedback</h1>
           <p className="font-inter text-[16px] lg:text-[32px] leading-[28px] lg:leading-[36px] text-white">각 팀에 대한 피드백을 남겨주세요</p>
         </div>
         <a href="https://forms.gle/3hpbVFAaaUVTLFQ29" className=" flex items-center justify-center w-[150px] h-[150px] lg:w-[200px] lg:h-[200px] rounded-[30px] bg-[#74CFFB] hover:scale-105">
